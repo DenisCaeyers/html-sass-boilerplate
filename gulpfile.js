5eda45d0215f59100cd342d501e1e212d7bbc812
Caeyers Denis <denis.caeyers@vito.be>
2019-08-08 10:31:23 +0200

    Added JS/IMG copy

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,19 @@ const browserSync = require("browser-sync").create();
 // variables
 var paths = {
   styles: {
-    src: "./assets/sass/**/*.scss",
-    dest: "./assets/css"
+    src: "./assets/public/sass/**/*.scss",
+    dest: "./assets/public/css"
   },
   html: {
     src: "./*.html"
+  },
+  js: {
+    src: "./assets/src/js/**/*.js",
+    dest: "./assets/public/js"
+  },
+  img: {
+    src: "./assets/src/img/**/*.*",
+    dest: "./assets/public/img"
   }
 };
 
@@ -30,6 +38,17 @@ function style() {
     .pipe(browserSync.stream());
 }
 
+function js() {
+  return gulp.src(paths.js.src).pipe(gulp.dest(paths.js.dest));
+}
+
+function img() {
+  return gulp
+    .src(paths.img.src)
+    .pipe(gulp.dest(paths.img.dest))
+    .pipe(browserSync.stream());
+}
+
 function watch() {
   browserSync.init({
     server: {
@@ -37,9 +56,16 @@ function watch() {
     }
   });
   style();
+  js();
+  img();
   gulp.watch(paths.styles.src, gulp.series(style));
+  gulp.watch(paths.js.src, gulp.series(js));
+  gulp.watch(paths.js.dest).on("change", browserSync.reload);
+  gulp.watch(paths.img.src, gulp.series(img));
   gulp.watch(paths.html.src).on("change", browserSync.reload);
 }
 
 exports.style = style;
+exports.js = js;
+exports.img = img;
 exports.watch = watch;
